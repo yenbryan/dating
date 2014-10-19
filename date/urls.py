1be8da2248f44545ab9c452b148fe50ac479ecd2
Peter <pwgraham91@gmail.com>
2014-10-19 16:45:00 -0700

    registration and pictures work

--- date/urls.py
+++ date/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from date import settings
-from date_app import urls as profile_urls
+# from date_app import urls as profile_urls
 
 urlpatterns = patterns('',
     #ADMIN#
